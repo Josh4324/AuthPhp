8b01ef8de7b44304e72f548e558881e4597a5dc4
Joshua Adesanya <adesanyajoshua@ymail.com>
2020-04-07 17:53:36 +0100

    authentication

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include_once("lib/header.php") ?>
+   <div>Welcome to SNG- Hospital for the ignorant</div> <br /> <hr />
+   <p>This is a specialist hospital to cure ignorance</p>
+   <p>Come as you are, it is completely free</p>
+
+<?php include_once("lib/footer.php") ?>
